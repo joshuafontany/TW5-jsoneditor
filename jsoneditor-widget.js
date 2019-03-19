64dbd319a0ecbea4e86dab33d3fdff43b15d6b55
Joshua Fontany <joshua.fontany@gmail.com>
2019-03-19 16:12:21 -0700

    keep hacking it

--- jsoneditor-widget.js
+++ jsoneditor-widget.js
@@ -22,6 +22,9 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 var JSONEditorWidget = function(parseTreeNode,options) {
   this.initialise(parseTreeNode,options);
+  this.options = {};
+  this.targets = [];
+  this.json = {};
 };
 
 /*
@@ -35,18 +38,15 @@ Render this widget into the DOM
 JSONEditorWidget.prototype.render = function(parent,nextSibling) {
   var self = this;
   this.parentDomNode = parent;
+  this.nextSibling = nextSibling;
   this.currentTiddler = this.getVariable("currentTiddler");
-  this.options = {};
-  this.domNode = $tw.utils.domMaker('div', {
-    document: this.document,
-    class: 'tw-jsoneditor'
-  });
   this.computeAttributes();
   this.execute();
-
+  this.cb = this.addSaveJsonCallback.bind(this);
+  this.editor.on("change", this.cb); //Always ignore first call to change callback
   // Insert element
-	parent.insertBefore(this.domNode,nextSibling);
-	this.renderChildren(this.domNode,null);
+	parent.insertBefore(this.domNode,this.nextSibling);
+	//this.renderChildren(this.domNode,null);
 	this.domNodes.push(this.domNode);
 }
 
@@ -55,98 +55,192 @@ Compute the internal state of the widget
 */
 JSONEditorWidget.prototype.execute = function() {
   /* Attributes are:
-  /  json = textReference to a json tiddler or an index in a json tiddler
-  /  schema  = textReference to a schema json (in a tiddler, field, or index)
   /  param = a valid json schema object that takes preference over the schema textReference
+  /  schema  = textReference to a schema json (in a tiddler, field, or index)
+  /  json = textReference to a json tiddler or an index in a json tiddler, defaults to schema+"/data"
   /  iconlib = the 'icon library' to use, must be one of validlibs[] below
   /  theme = one of the validthemes[] below
-  /*/
+  */
+  this.schemaObj = this.getAttribute("param", "");
+  this.schemaRef = this.getAttribute("schema", "New Schema Tiddler");
   this.jsonRoot = this.getAttribute("json", "New Json Tiddler");
-  this.schemaRef = this.getAttribute("schema", this.jsonRoot);
-  this.schemaObj = this.getAttribute("param");
-  // An attribute named 'param' can pass in a valid schema json object
-  // (triple quoted for multi-line), or as translusion, variable, etc.
-  // Otherwise 'schemaRef' contains a textReference from which to retreive
-  // the json schema definition. Defaults to '{}' it still works with that degenerate case.
-  var text = this.wiki.getTextReference(this.schemaRef, "{}", this.currentTiddler),
-  schemaValid = $tw.utils.jsonIsValid(text, this.schemaRef) || false,
-  paramValid = $tw.utils.jsonIsValid(this.schemaObj, this.currentTiddler) || false;
-  if ((!this.schemaObj || this.schemaObj === "") && paramValid) {
-    this.options.schema = this.schemaObj;
-  }
-  else if (this.schemaRef && schemaValid)
-  { this.options.schema = JSON.parse(text); }
-  else { this.options.schema = {}; }
-  //Test to see if the jsonRoot is a tiddler title or a reference to a field or index
-  var result = $tw.utils.parseTextReference(this.jsonRoot);
-  if(!$tw.wiki.tiddlerExists(result.title)) {
-    $tw.wiki.setTextReference(this.jsonRoot, "{}");
-  }
-  if (result.field) { //If json target is a field, pre-load values
-      this.options.startval = this.json;
-      this.target = "field";
-  }
-  else if (result.index) this.target = "index";
-  else this.target = "tiddler";
-  this.options.target = this.target;
-  this.options.form_name_root = this.jsonRoot;
-  
-  //If jsonRoot contains invalid json, alert
-  var jsonstring = $tw.wiki.getTextReference(this.jsonRoot, "{}", this.currentTiddler);
-  if($tw.utils.jsonIsValid(result.title, jsonstring)) {
-    this.json = JSON.parse(jsonstring);
-  }
-  else {
-    this.makeChildWidgets(["''Error: invalid json attribute''"]);
-    return; 
-  }
-  // iconlib and theme
-  var lib = this.getAttribute("iconlib");
-  var validlibs = [
-    "bootstrap2",
-    "bootstrap3",
-    "foundation2",
-    "foundation3",
-    "jqueryui",
-    "fontawesome3",
-    "fontawesome4",
-    "fontawesome5",
-    "materialicons"
-  ];
-  if (validlibs.indexOf(lib) != -1 ) this.options.iconlib = lib;
-  var th = this.getAttribute("theme");
-  var validthemes = [
-    "barebones",
-    "html",
-    "bootstrap2",
-    "bootstrap3",
-    "bootstrap4",
-    "foundation3",
-    "foundation4",
-    "foundation5",
-    "foundation6",
-    "jqueryui",
-    "materialize"];
-  if (validthemes.indexOf(th) != -1 ) this.options.theme = th;
+  this.targets = this.setTargets();
+  this.json = this.getJsonFromAttributes();
+  this.options = this.getOptionsFromAttributes();
   // Create root editor
-  this.options.parent = this;
-  this.editor = new JSONEditor(this.domNode, this.options);
+  var container = this.document.createElement('div');
+  container.setAttribute("class", "tw-jsoneditor");
+  this.editor = new JSONEditor(container, this.options);
   // Workaround for what I think is a bug in jsoneditor
   if (_.isEmpty(this.options.schema) && (this.options.startval == 0)) this.editor.setValue(this.options.startval);
-	// Construct the child widgets
-	this.makeChildWidgets(this.editor.jsoneditor.root_container);
+  if(this.targets[1].type !== "field") 
+  { //Replace inputs with TW widgets
+    this.rebuildEditorNodes(container);
+  }  
+   // Construct the child widgets
+  this.domNode = container;
+	this.makeChildWidgets(this.domNode);
 };
+
+/*
+Sets the target attribute based on the json textReference
+*/
+JSONEditorWidget.prototype.setTargets = function() {
+  var sources = [],
+  results = [],
+  targets = [];
+  sources[0] = this.schemaRef;
+  sources[1] = this.jsonRoot;
+  results[0] = $tw.utils.parseTextReference(sources[0]);
+  results[1] = $tw.utils.parseTextReference(sources[1]);
+  var objKeys = Object.keys(results);
+  objKeys.forEach(function(i) {
+    var result = results[i];
+    if (result.field) { 
+      targets[i] = {type: "field"};
+    }
+    else if (result.index) targets[i] = {type: "index"};
+    else targets[i] = {type: "tiddler"};
+    targets[i].title = result.title;
+    targets[i].field = result.field || "";
+    targets[i].index = result.index || "";
+  }, this);
+  return targets;
+}
+
+/*
+Rebuilds the options object
+*/
+JSONEditorWidget.prototype.getOptionsFromAttributes = function() {
+  var optionstring = this.getAttribute("options", "{}"),
+  optionsValid = $tw.utils.jsonIsValid(this.currentTiddler, optionstring) || false;
+  options = optionsValid ? SON.parse(optionstring) : {};
+  /* An attribute named 'param' can pass in a valid schema json string
+    /  as a triple quoted string, or as translusion, variable, etc.
+    /  Otherwise 'schema' contains a textReference from which to retreive
+    /  the json schema definition. Defaults to '{}' as it still works with 
+    /  that degenerate case.
+    */
+  var text = this.wiki.getTextReference(this.schemaRef, "{}", this.currentTiddler),
+  schemaValid = $tw.utils.jsonIsValid(this.targets[0].title, text) || false,
+  paramValid = (this.schemaObj && this.schemaObj !== "") ? $tw.utils.jsonIsValid(this.currentTiddler, this.schemaObj) : false;
+  if (paramValid) {
+    options.schema = JSON.parse(this.schemaObj);
+  }
+  else if (this.schemaRef && schemaValid)
+  { options.schema = JSON.parse(text); }
+  if(!options.schema || _.isEmpty(options.schema) || !options.schema.type) options.schema = {};
+  //Test to see if the jsonRoot is a tiddler title or a reference to a field or index
+  options.target = this.targets[1];
+  options.form_name_root = this.jsonRoot;
+   // iconlib and theme
+   var lib = options.iconlib;
+   var validlibs = [
+     "bootstrap2",
+     "bootstrap3",
+     "foundation2",
+     "foundation3",
+     "jqueryui",
+     "fontawesome3",
+     "fontawesome4",
+     "fontawesome5",
+     "materialicons"
+   ];
+   options.iconlib = (validlibs.indexOf(lib) != -1 ) ? lib : "";
+   var th = this.getAttribute("theme");
+   var validthemes = [
+     "barebones",
+     "html",
+     "bootstrap2",
+     "bootstrap3",
+     "bootstrap4",
+     "foundation3",
+     "foundation4",
+     "foundation5",
+     "foundation6",
+     "jqueryui",
+     "materialize"];
+   options.theme = (validthemes.indexOf(th) != -1 ) ? th : "";
+  //If json target is a field, pre-load values
+  options.startval = (this.targets[1].type == "field") ? this.json : null;
+  return options;
+}
+
+/*
+Rebuilds the json object from source
+*/
+JSONEditorWidget.prototype.getJsonFromAttributes = function() {
+  var jsonstring = this.wiki.getTextReference(this.jsonRoot, "", this.currentTiddler);
+  //If jsonRoot contains invalid json, alert
+  if($tw.utils.jsonIsValid(this.targets[1].title, jsonstring) && jsonstring !== ""){
+    return JSON.parse(jsonstring);
+  }
+} 
+
+/*
+Rebuilds the domNode if the target is a json tiddler or index
+*/
+JSONEditorWidget.prototype.rebuildEditorNodes = function(domNode) {
+//
+domNode.appendChild(this.document.createTextNode("**I should have widgets!**"));
+return domNode;
+}
+
+/*
+Diff and save Json and Schema on each callback change.
+*/
+JSONEditorWidget.prototype.saveJson = function() {
+  var editorValue = this.editor.getValue();
+  //var editorSchema = this.editor.;
+  if (!_.isEqual(this.json, editorValue)) {
+    this.wiki.setTextReference(this.jsonRoot, JSON.stringify(this.editor.getValue()), this.getVariable("currentTiddler"));
+  }
+  // if (!_.isEqual(this.options.schema, editorSchema)) {
+  //   this.wiki.setTextReference(this.jsonRoot, JSON.stringify(this.editor.getValue()), this.getVariable("currentTiddler"));
+  // }
+}
+JSONEditorWidget.prototype.addSaveJsonCallback = function() {
+  var self=this;
+  this.editor.off("change", this.cb);
+  this.editor.on("change", function() {
+    self.saveJson();
+  }); // autosave changes
+}
 
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
 JSONEditorWidget.prototype.refresh = function(changedTiddlers) {
   var changedAttributes = this.computeAttributes();
-  if(changedAttributes.schema || changedAttributes.theme || changedAttributes.json || changedAttributes.param) {
-		this.refreshSelf();
-		return true;
-	}
-	return this.refreshChildren(changedTiddlers);
+  var targets = this.setTargets();
+  if (changedAttributes.param || changedAttributes.schema || changedAttributes.json || changedAttributes.options) {
+    var self=this;
+    this.editor.off("change");
+    this.editor.destroy();
+    this.refreshSelf();
+    return true;
+  }
+  else if (changedTiddlers[targets[0].title] || changedTiddlers[targets[1].title]){
+    if(this.targets[1].type !== "field"){
+      var self=this;
+      this.editor.off("change");
+      this.editor.destroy();
+      this.refreshSelf();
+      return true;
+    }
+    else{
+      var options = this.getOptionsFromAttributes(),
+          json = this.getJsonFromAttributes();
+      var jsonEq = _.isEqual(this.json, json),
+          opEq = _.isEqual(this.options, options);
+      if(!jsonEq) {this.json = json; this.editor.root.value = json;}
+      if(!opEq) this.editor.options = options;
+      if (!jsonEq || !opEq) this.editor.trigger("change");
+    }
+  }
+  if (this.targets[1].type !== "field"){
+    return this.refreshChildren(changedTiddlers);
+  }
 };
 
 exports.jsoneditor = JSONEditorWidget;
