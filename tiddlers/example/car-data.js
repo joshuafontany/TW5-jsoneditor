33fe1f96ad8e5a7e503bb1f625a1c994b0a48d4a
Joshua Fontany <joshua.fontany@gmail.com>
2019-08-11 21:31:01 -0700

    reorg

--- tiddlers/example/car-data.js
+++
@@ -1,3 +0,0 @@
-//Comments
-{"title": "Js Tiddler storing schema and json data in fields so the text field can be used for other things."}
-
---
+++ tiddlers/wiki/example/car-data.js
@@ -0,0 +1,3 @@
+//Comments
+{"title": "Js Tiddler storing schema and json data in fields so the text field can be used for other things."}
+
